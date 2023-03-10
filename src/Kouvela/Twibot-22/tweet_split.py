0810935d58064ca4c17a0521947078706c0d191a
Manuel <manuel.tonneau@mailbox.org>
2023-03-10 09:55:28 +0000

    do only tweet split and add prints

--- src/Kouvela/Twibot-22/tweet_split.py
+++ src/Kouvela/Twibot-22/tweet_split.py
@@ -14,7 +14,7 @@ def generate_userful(num):
     print('Read tweet_{} finish:'.format(num), time.time() - start)
 
     df = pd.DataFrame(columns = ['id','text','like','retweet'])
-    for i in tqdm(range(length)):
+    for i in range(length):
         content = {}
         content['id'] = t[i]['id']
         content['text'] = t[i]['text']
@@ -30,18 +30,20 @@ if __name__ == '__main__':
     init_tweet = locals()
     code_path = '/scratch/mt4493/bot_detection/code/TwiBot-22/src/Kouvela'
     for i in range(9):
+        print(f'loading file {i}')
         init_tweet['t{}'.format(i)] = generate_userful(i)
 
     final = pd.concat([t0, t1, t2, t3, t4, t5, t6, t7, t8], axis=0)
     final = final.fillna(0)
     final.sort_values(by="id", axis=0, ascending=True, inplace=True, ignore_index=True)
-
+    print(f'len(final): {len(final)}')
     # the initial 9 tweets_i.json files are splited into some small csv files
     # in order to speed up content feature extraction
     num = 1000000
     batch = int(len(final) / num)
 
     os.makedirs(os.path.join(code_path, 'Twibot-22', 'small_split_dataset'))
+    print('starting to save csvs')
     for i in tqdm(range(batch)):
         begin = i * num
         tweet = final.loc[begin : begin + num - 1]
